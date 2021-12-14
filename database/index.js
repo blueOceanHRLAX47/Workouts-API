16e89c6ef69cd884f32eb7515338314e3b7815e1
Timmy Liaw <timmy1544@gmail.com>
2021-12-14 12:31:11 -0800

    update ip

--- database/index.js
+++ database/index.js
@@ -1,7 +1,7 @@
 const { Sequelize, DataTypes } = require('sequelize');
 
 const sequelize = new Sequelize('fbc', 'postgres', 'postgres', {
-  host: '35.236.62.7',
+  host: '10.49.144.4',
   dialect: 'postgres'
 });
 
